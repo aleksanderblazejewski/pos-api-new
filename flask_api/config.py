fcc15a97c656796adba3335270607a747bae7988
aleksanderblazejewski <120112652+aleksanderblazejewski@users.noreply.github.com>
2026-01-27 14:14:51 +0100

    Update config.py

--- flask_api/config.py
+++ flask_api/config.py
@@ -1,11 +1,12 @@
 import os
 
+
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
         "mysql+pymysql://admin:Admin123!@localhost/restauracja"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "change-me")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "4ac3d303fb8e777c82192b7361d76768f03f133497053f5d506e3470f785d30d")
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
     JWT_EXPIRES_SECONDS = int(os.getenv("JWT_EXPIRES_SECONDS", "3600"))
